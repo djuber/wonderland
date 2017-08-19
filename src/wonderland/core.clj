76e1402530b09d38e325246f2596911c2ccabfcf
Daniel Uber <djuber@gmail.com>
2017-08-19 14:21:09 -0500

    catchup

--- src/wonderland/core.clj
+++ src/wonderland/core.clj
@@ -5,21 +5,7 @@
   [x]
   (println x "Hello, World!"))
 
-(defn marity
-  "example of pattern match dispatch"
-  ([x] x)
-  ([x y] (+ x y))
-  ([x y & more] (apply + x y more)))
-
-(defn never [& _]
-  "constantly false"
-  false)
-
-(defn always [& _]
-  "constantly true"
-  true)
-
-
-(defn ->vector [aseq]
-  "if a seq is returned, enforce a vector"
-  (reduce #(conj %1 %2) [] aseq))
+(defn shop-for-jams [jam1 jam2]
+  {:name "jam basket"
+   :jam1 jam1
+   :jam2 jam2})
