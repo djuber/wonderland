34291c101e9ea87935fbab56e4b12db2692380f1
Dan Uber <djuber@gmail.com>
2017-08-19 11:21:33 -0500

    cleaned up docstrings to use clojure norms.

--- src/wonderland/euler/one.clj
+++ src/wonderland/euler/one.clj
@@ -1,46 +1,68 @@
 (ns wonderland.euler.one)
 
-(defn divides? [a b]
+;; just trying to build up enough vocabulary to do this when I don't know clojure yet.
+
+(defn divides?
   "true when a divides b"
+  [a b]
   (= 0 (mod b a)))
 
-(defn divisor? [a]
+(defn divisor?
   "yields a predicate for divides?"
+  [a]
   (fn [n] (divides? n a)))
 
-(defn any? [pred seq]
+(defn any?
   "pred true for any in seq?"
+  [pred seq]
   (some true?
         (map pred seq)))
 
-(defn any-divide? [a bs]
+(defn any-divide?
   "true when there is a divisor of a in bs"
+  [a bs]
   (any? (divisor? a) bs))
 
-(defn upto [limit]
+(defn upto
   "gives 0 upto limit exclusive"
+  [limit]
   (take-while #(< %1 limit)
               (range)))
 
-(defn upto-natural [limit]
+(defn upto-natural
   "gives 1 upto limit exclusive"
+  [limit]
   (drop 1 (upto limit)))
 
-(defn is-multiple? [a b]
+(defn is-multiple?
   "true when a is a multiple of b"
+  [a b]
   (= 0 (mod a b)))
 
-(defn multiple? [b]
+(defn multiple?
+  "yields a predicate to test for multiple of input"
+  [b]
   (fn [a] (is-multiple? a b)))
 
-(defn any-of? [preds x]
+(defn any-of?
   "x when any of preds are true on it"
+  [preds x]
   (if (any? #(true? (%1 x)) preds) x))
 
-(defn multiples-below [limit factors]
+(defn multiples-below
+  "collect multiples of factors from 1 below limit"
+  [limit factors]
   (remove nil?
           (map (fn [n] (any-of? (map multiple? factors) n))
                (upto-natural limit))))
 
-(defn sum-of-multiples-below [limit factors]
-  (reduce + (multiples-below limit factors)))
+(defn sum-of-multiples-below
+  "add the multiples below limit of factors"
+  [limit factors]
+  (reduce +
+          (multiples-below limit factors)))
+
+(defn euler-one
+  "https://projecteuler.net/problem=1"
+  []
+  (sum-of-multiples-below 1000 [3 5]))
