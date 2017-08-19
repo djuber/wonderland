de305d12f6c6c6e004f9987d03a9e09d751e4074
Dan Uber <djuber@gmail.com>
2017-08-18 21:39:55 -0500

    Initial checkin of clojure experiments.

---
+++ src/wonderland/core.clj
@@ -0,0 +1,25 @@
+(ns wonderland.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(defn marity
+  "example of pattern match dispatch"
+  ([x] x)
+  ([x y] (+ x y))
+  ([x y & more] (apply + x y more)))
+
+(defn never [& _]
+  "constantly false"
+  false)
+
+(defn always [& _]
+  "constantly true"
+  true)
+
+
+(defn ->vector [aseq]
+  "if a seq is returned, enforce a vector"
+  (reduce #(conj %1 %2) [] aseq))
---
+++ test/wonderland/core_test.clj
@@ -0,0 +1,31 @@
+(ns wonderland.core-test
+  (:require [clojure.test :refer :all]
+            [wonderland.core :refer :all]))
+
+(deftest a-test
+  (testing "I pass."
+    (is (=  1 1))))
+
+(deftest never-is-always-false
+  (testing "never is always false"
+    (is (false? (never)))
+    (is (false? (never 1)))
+    (is (false? (never 1 2 3)))
+    (is (false? (never true)))
+    (is (false? (never (never))))))
+
+(deftest always-is-always-true
+  (testing "always is always true"
+    (is (true? (always)))
+    (is (true? (always nil)))
+    (is (true? (always true)))
+    (is (true? (always 1 2 3 4)))
+    (is (true? (always (never))))))
+
+(deftest ->vector-makes-vectors
+  (testing "->vector yields a new vector"
+    (is (= [] (->vector [])))
+    (is (contains? (->vector #{1 2 3}) 2))
+    (is (= [1 2 3] (->vector [1 2 3])))
+    (is (= [1 2 3] (->vector (list 1 2 3))))
+    (is (= [1 2 '(1 2)] (->vector (list 1 2 (list 1 2)))))))
