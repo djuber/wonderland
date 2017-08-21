dccf50d8c472ab9c91c99f59524bfc82f215574b
Dan Uber <djuber@gmail.com>
2017-08-21 08:39:29 -0500

    problem five solution.
    Implemented lcm and gcd. Need to move these to core utilities.

--- src/wonderland/euler/five.clj
+++ src/wonderland/euler/five.clj
@@ -6,6 +6,26 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 "
 
+(defn gcd [a b]
+  (if (zero? b)
+    a
+    (recur b (mod a b))))
+
+(defn two-arg-lcm
+  "this two-arg and lcm combination comes from sbcl's source code."
+  [a b]
+  (if (or (zero? a) (zero? b))
+    0
+    (let [a (Math/abs a)
+           b (Math/abs b)]
+      (let [m (max a b)
+             n (min a b)]
+        (* (quot m (gcd a b)) n)))))
+
 (defn lcm
   "give the least common multiple of arguments"
-  [numbers])
+  [numbers]
+  (reduce two-arg-lcm numbers))
+
+(defn euler-five []
+  (lcm (wonderland.euler.core/upto-natural 20)))
